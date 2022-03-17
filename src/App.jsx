320a9d027deccfb670b9e405c0e5875ddad04aae
jobaji09 <jobaji09@gmail.com>
2022-03-16 19:17:39 -0600

    add de list of the members

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import friends from '/assets/friends.jpg'
-import facultaCiencias from '/assets/Escudo-facultad-ciencias-unam-escalable.svg'
+import daniela from '/assets/Daniela.jpeg'
+import luis from '/assets/Luis.jpeg'
+import jonathan from '/assets/jonathan.png'
 import './App.css'
 
 function App() {
@@ -57,7 +59,49 @@ function App() {
             <span>
               Si te interesa saber mas de cada uno te invito a checar el portafolio de cada uno.
             </span>
+            <div className='contact-us'>
+              <div className='contact'>
+                <h3>Daniela Calderón Pérez </h3>
+                <img
+                  alt="Daniela Calderon Perez"
+                  className='contact-image'
+                  src={daniela}
+                />
+                <ul className='social-media-list'>
+                  <li><a href="https://www.linkedin.com/in/daniela-calder%C3%B3n-p%C3%A9rez-87998221a/">linkedin</a></li>
+                  <li><a href="https://github.com/DanielaCP19">github</a></li>
+                </ul>
+              </div>
+              <div className='contact'>
+                <h3>Jonathan Barragán Jiménez</h3>
+                <img
+                  alt="Jonathan Barragán Jiménez"
+                  className='contact-image'
+                  src={jonathan}
+                />
+                <ul className='social-media-list'>
+                  <li><a href="https://www.linkedin.com/in/jonathan-barrag%C3%A1n-68a76b200/">linkedin</a></li>
+                  <li><a href="https://github.com/jobaji09">github</a></li>
+                </ul>
+              </div>
+              <div className='contact'>
+                <h3>Luis Soto Martínez</h3>
+                <img
+                  alt="Luis Soto Martínez"
+                  className='contact-image'
+                  src={luis}
+                />
+                <ul className='social-media-list'>
+                  <li><a href="https://www.linkedin.com/in/luis-soto-mart%C3%ADnez-8604ab127/">linkedin</a></li>
+                  <li><a href="https://github.com/luisoto076">github</a></li>
+                </ul>
+              </div>
+            </div>
           </div>
+          <div className='card-info'>
+         
+          </div>
+          
         </section>
 
 
