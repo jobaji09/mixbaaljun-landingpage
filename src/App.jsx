44f757fd1403c769ac9595ba26695004e8f7a209
jobaji09 <jobaji09@gmail.com>
2022-03-21 22:09:01 -0600

    Changing the layout to be more lean

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import friends from '/assets/friends.jpg'
 import daniela from '/assets/Daniela.jpeg'
 import luis from '/assets/Luis.jpeg'
-import jonathan from '/assets/jonathan.png'
+import jonathan from '/assets/Jonathan.jpeg'
 import './App.css'
 
 function App() {
@@ -12,105 +12,76 @@ function App() {
     <div className="App">
       <header>
         <nav>
-          <h1>MixbaalJun</h1>
+          <h1>MixbáalJun</h1>
         </nav>
       </header>
 
       <main>
-
-        <section className='card'>
-          <div className='card-content'>
-            <h2 className='frase title'>
-              Hola, Somos MixbaalJun
-            </h2>
-            <span>
+        <div className='welcome'>
+          <div className='welcome-title'>
+            <span className='title'>
+              Hola, Somos MixbáalJun
+            </span>
+            <span className='welcome-description'>
               Este proyecto surgió mientras éramos estudiantes de Ciencias de la Computación en la máxima casa de estudios UNAM,
               lo que empezó como un proyecto escolar se convirtió en una plataforma para proyectar nuestros trabajos personales y
-              los proyectos que seguimos realizando bajo el sello de MixbaalJun, acompáñanos a conocer más de cada uno de nosotros.
+              los proyectos que seguimos realizando bajo el sello de MixbáalJun, ¿Te animas conocernos más?.
             </span>
-
           </div>
-          <div className='card-info'>
-            <img src={friends} alt="friends" className='card-image' />
+
+
+          <div className='welcome-image'>
+            <img src={friends} alt="friends" />
           </div>
-        </section>
+        </div>
 
-
-        {/* <section className='card'>
-
-          <div className='card-content'>
-            <h2 className='frase'>
-              Computologos de corazón
-            </h2>
-            <span>o de nuestros integrantes
-            </span>
-
+        <div className='contact-us'>
+          <div className='contact'>
+            <h3>Daniela Calderón Pérez </h3>
+            <img
+              alt="Daniela Calderon Perez"
+              className='contact-image'
+              src={daniela}
+            />
+            <ul className='social-media-list'>
+              <li><a href="https://www.linkedin.com/in/daniela-calder%C3%B3n-p%C3%A9rez-87998221a/">linkedin</a></li>
+              <li><a href="https://github.com/DanielaCP19">github</a></li>
+            </ul>
           </div>
-          <div className='card-info'>
-            <img src={facultaCiencias} alt="Escudo facultad de ciencias" className='card-image' />
+          <div className='contact'>
+            <h3>Jonathan Barragán Jiménez</h3>
+            <img
+              alt="Jonathan Barragán Jiménez"
+              className='contact-image'
+              src={jonathan}
+            />
+            <ul className='social-media-list'>
+              <li><a href="https://www.linkedin.com/in/jonathan-barrag%C3%A1n-68a76b200/">linkedin</a></li>
+              <li><a href="https://github.com/jobaji09">github</a></li>
+            </ul>
           </div>
-        </section> */}
-
-        <section className='card'>
-          <div className='card-content'>
-            <h2 className='frase'>
-              Conocenos a fondo
-            </h2>
-            <span>
-              Si te interesa saber mas de cada uno te invito a checar el portafolio de cada uno.
-            </span>
-            <div className='contact-us'>
-              <div className='contact'>
-                <h3>Daniela Calderón Pérez </h3>
-                <img
-                  alt="Daniela Calderon Perez"
-                  className='contact-image'
-                  src={daniela}
-                />
-                <ul className='social-media-list'>
-                  <li><a href="https://www.linkedin.com/in/daniela-calder%C3%B3n-p%C3%A9rez-87998221a/">linkedin</a></li>
-                  <li><a href="https://github.com/DanielaCP19">github</a></li>
-                </ul>
-              </div>
-              <div className='contact'>
-                <h3>Jonathan Barragán Jiménez</h3>
-                <img
-                  alt="Jonathan Barragán Jiménez"
-                  className='contact-image'
-                  src={jonathan}
-                />
-                <ul className='social-media-list'>
-                  <li><a href="https://www.linkedin.com/in/jonathan-barrag%C3%A1n-68a76b200/">linkedin</a></li>
-                  <li><a href="https://github.com/jobaji09">github</a></li>
-                </ul>
-              </div>
-              <div className='contact'>
-                <h3>Luis Soto Martínez</h3>
-                <img
-                  alt="Luis Soto Martínez"
-                  className='contact-image'
-                  src={luis}
-                />
-                <ul className='social-media-list'>
-                  <li><a href="https://www.linkedin.com/in/luis-soto-mart%C3%ADnez-8604ab127/">linkedin</a></li>
-                  <li><a href="https://github.com/luisoto076">github</a></li>
-                </ul>
-              </div>
-            </div>
+          <div className='contact'>
+            <h3>Luis Soto Martínez</h3>
+            <img
+              alt="Luis Soto Martínez"
+              className='contact-image'
+              src={luis}
+            />
+            <ul className='social-media-list'>
+              <li><a href="https://www.linkedin.com/in/luis-soto-mart%C3%ADnez-8604ab127/">linkedin</a></li>
+              <li><a href="https://github.com/luisoto076">github</a></li>
+            </ul>
           </div>
-          <div className='card-info'>
-         
-          </div>
-          
-        </section>
+        </div>
+    
 
 
-      </main>
+      </main >
 
-      <footer>
-        <span>No hay derechos que reservar gracias</span>
-      </footer>
-    </div>
+    <footer>
+      <span>No hay derechos que reservar gracias</span>
+    </footer>
+    </div >
   )
 }
 
